b0544f39d949c9f2886698836782909b8c8a7183
Doug Franklin <doug@dougfranklin.com>
2018-08-12 09:55:44 -0400

    home-routing.module.ts wasn't aligned properly

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,34 +1,31 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomePage }     from './home.page';
+import { HomePage } from './home.page';
 import { LocationPage } from '../location/location.page';
+import { CampDetailsPage } from '../camp-details/camp-details.page';
+import { MyDetailsPage } from '../my-details/my-details.page';
 
-const routes: Routes = [{
-    path: 'tabs',
-    component: HomePage,
-    children: [
-        {
-            path: 'tabs',
-            component: HomePage,
-            children: [
-                {
-                    path: 'location',
-                    outlet: 'location',
-                    component: LocationPage
-                }
-            ]
-        },
-        {
-            path: '',
-            redirectTo: '/tabs/(location:location)'
-        }
-    ]
-}];
+const routes: Routes = [
+    { 
+        path: 'tabs', 
+        component: HomePage,
+        children: [
+            {
+                path: 'location',
+                outlet: 'location',
+                component: LocationPage
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/tabs/(location:location)'
+    }
+];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
