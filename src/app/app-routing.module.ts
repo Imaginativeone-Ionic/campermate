08fcd171dbf74ee1c41c11772fa04eea522f8c78
Doug Franklin <doug@dougfranklin.com>
2018-08-12 07:04:48 -0400

    src/app/app-routing.module.ts Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'CampDetails', loadChildren: './camp-details/camp-details.module#CampDetailsPageModule' },
-  { path: 'MyDetails', loadChildren: './my-details/my-details.module#MyDetailsPageModule' },
-  { path: 'Location', loadChildren: './location/location.module#LocationPageModule' },
-  { path: 'GoogleMap', loadChildren: './components/google-map/google-map.module#GoogleMapPageModule' },
+  { path: '', loadChildren: './home/home.module#HomePageModule' }
 ];
 
 @NgModule({
